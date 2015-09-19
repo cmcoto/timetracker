c290bce56d1ade352577309cd3e1cc2674628f2e
Carlos Coto <cmcoto@gmail.com>
2015-09-19 11:05:54 -0600

    Implement Subdomains, need to implement differentiation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
-Rails.application.routes.draw do
-  
-  devise_for :users
-  root to: 'welcome#index'
-  
-  resources :accounts
+class SubdomainPresent
+	def self.matches?(request)
+		request.subdomain.present?
+	end
+end
 
+class SubdomainBlank
+  def self.matches?(request)
+    request.subdomain.blank?
+  end
+end
+
+Timetracker::Application.routes.draw do
+	constraints(SubdomainPresent) do
+		devise_for :users
+	end
+
+	constraints(SubdomainBlank) do
+		root to: 'welcome#index'
+	    resources :accounts, only: [:new, :create]
+	end
 end
--- spec/features/authentication_feature_spec.rb
+++ spec/features/authentication_feature_spec.rb
@@ -1,31 +1,35 @@
 require 'spec_helper'
 
 describe 'user_authentication'  do   
+  let(:user) { build(:user) }
+  let!(:account) { create(:account, owner: user) }
+
   it 'allows signin with valid credentials' do
-    user = create(:user)
-    sign_user_in(user)
+    sign_user_in(user, subdomain: account.subdomain)
     expect(page).to have_content('Signed in successfully.')
   end
 
   it 'does not allow signin with invalid credentials' do
-    user = create(:user)
-    sign_user_in(user, password: 'wrong pw')
+    sign_user_in(user, subdomain: account.subdomain, password: 'wrong pw')
 
     expect(page).to have_content('Invalid email or password')
   end
 
-  it 'allows user to sign out' do
-    user = create(:user)
-    sign_user_in(user)
+  it 'does not allow user to sign in unless on subdomain' do
+    expect{ visit new_user_session_path }.to raise_error ActionController::RoutingError
+  end
 
-    visit root_path
+  it 'allows user to sign out' do
+    sign_user_in(user, subdomain: account.subdomain)
+
+ 
     click_link 'Sign out'
     expect(page).to have_content('Signed out successfully.')
   end
 end
 
 def sign_user_in(user, opts={})
-  visit new_user_session_path
+  visit new_user_session_url(subdomain: opts[:subdomain])
   fill_in 'Email', with: user.email
   fill_in 'Password', with: (opts[:password] || user.password)
   click_button 'Sign in'
